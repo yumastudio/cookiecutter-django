ee61e0af28206fd5c25519f3dc5f357a87dff332
Gabriel Sebag <gabriel.sebag@efrei.net>
2020-11-30 18:31:28 +0100

    Correcting issues with virtual hosts

--- {{cookiecutter.project_slug}}/django/config/settings/local.py
+++ {{cookiecutter.project_slug}}/django/config/settings/local.py
@@ -12,7 +12,7 @@ SECRET_KEY = env(
     default="!!!SET DJANGO_SECRET_KEY!!!",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["localhost", "0.0.0.0", "127.0.0.1"])
 
 # CACHES
 # ------------------------------------------------------------------------------
