c0d5b67b8f0a15d1eeef5731619fcba67903a46a
Gabriel Sebag <gabriel.sebag@efrei.net>
2020-11-30 19:35:59 +0100

    Solved issues with user tests

--- {{cookiecutter.project_slug}}/django/config/testing.py
+++ {{cookiecutter.project_slug}}/django/config/testing.py
@@ -1,14 +1,17 @@
 import json
 import logging
-from graphene_django.utils.testing import GraphQLTestCase as GQLTestCase
-from django.test.utils import override_settings
 from .schema import schema
+from django.test import override_settings
+from graphene_django.utils.testing import GraphQLTestCase as GQLTestCase
 from {{cookiecutter.project_slug}}.users.tests.factories import (
     UserFactory
 )
-from datetime import timedelta
 
 
+@override_settings(GRAPHQL_JWT={
+    'JWT_AUTH_HEADER_NAME': 'Authorization',
+    'JWT_AUTH_HEADER_PREFIX': 'Bearer'
+})
 class GraphQLTestCase(GQLTestCase):
     GRAPHQL_SCHEMA = schema
     GRAPHQL_URL = "/"
@@ -17,7 +20,7 @@ class GraphQLTestCase(GQLTestCase):
     def tearDown(self):
         UserFactory.reset_sequence()
 
-    def logging(self, query, result, variables={}):
+    def _logging(self, query, result, variables={}):
         logging.info("""
             Query: {query}
             Variables: {variables}
@@ -28,19 +31,11 @@ class GraphQLTestCase(GQLTestCase):
             result=result
         ))
 
-    def set_headers(self, token=None):
+    def _set_token(self, token=None):
         self.headers = {
             'Authorization': 'Bearer ' + token
         } if token else {}
 
-    @override_settings(DEBUG=True, GRAPHQL_JWT={
-        'JWT_SECRET_KEY': 'KEY',
-        'JWT_VERIFY_EXPIRATION': True,
-        'JWT_AUTH_HEADER_NAME': 'Authorization',
-        'JWT_AUTH_HEADER_PREFIX': 'Bearer',
-        'JWT_EXPIRATION_DELTA': timedelta(days=30),
-        'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=365),
-    })
     def _execute_query(self, query, op_name, variables={}, should_fail=False):
         logging.disable(logging.CRITICAL)
         response = self.query(
--- {{cookiecutter.project_slug}}/django/{{cookiecutter.project_slug}}/users/tests/test_graphql.py
+++ {{cookiecutter.project_slug}}/django/{{cookiecutter.project_slug}}/users/tests/test_graphql.py
@@ -49,7 +49,7 @@ class UserTestCase(GraphQLTestCase):
             **kwargs
         )
 
-    def login(self, **kwargs):
+    def login_email(self, **kwargs):
         query = '''
             mutation loginEmail(
                 $username: String!,
@@ -94,11 +94,47 @@ class UserTestCase(GraphQLTestCase):
         variables = {
             'email': "email@email.com",
             'password': "password",
-            'full_name': 'John Doe'
+            'fullName': 'John Doe'
         }
-        self.logging(
-            query="seoCities",
+        self._logging(
+            query="createUser",
             variables=variables,
             result="The user is created"
         )
         self.create_user(variables=variables)
+
+        logging.info("* I want to login with my new user.")
+        variables = {
+            'username': "email@email.com",
+            'password': "password2"
+        }
+        self._logging(
+            query="loginEmail",
+            variables=variables,
+            result="I can't login because I set a wrong password"
+        )
+        self.login_email(variables=variables, should_fail=True)
+
+        variables = {
+            'username': "email@email.com",
+            'password': "password"
+        }
+        self._logging(
+            query="loginEmail",
+            variables=variables,
+            result="I login successfully now that I put the right password"
+        )
+        response = self.login_email(variables=variables)
+
+        logging.info("* I now want to get my profile.")
+        self._logging(
+            query="userProfile",
+            result="It should fail because I didn't put my token"
+        )
+        self.user_profile(should_fail=True)
+        self._set_token(token=response['token'])
+        self._logging(
+            query="userProfile",
+            result="It should work because I added my token in the headers"
+        )
+        self.user_profile()
