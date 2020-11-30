c62c093ec9202f855e16d113c289fb8cfdd81fcf
Gabriel Sebag <gabriel.sebag@efrei.net>
2020-11-30 22:15:56 +0100

    Generator is working locally

--- {{cookiecutter.project_slug}}/django/{{cookiecutter.project_slug}}/users/apps.py
+++ {{cookiecutter.project_slug}}/django/{{cookiecutter.project_slug}}/users/apps.py
@@ -5,3 +5,12 @@ from django.utils.translation import gettext_lazy as _
 class UsersConfig(AppConfig):
     name = "{{ cookiecutter.project_slug }}.users"
     verbose_name = _("Users")
+
+    def ready(self):
+        try:
+            from easy_thumbnails.signals import saved_file
+            from easy_thumbnails.signal_handlers import generate_aliases_global
+
+            saved_file.connect(generate_aliases_global)
+        except ImportError:
+            pass
